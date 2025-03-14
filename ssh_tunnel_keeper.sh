a9a53960c235233a10aea46025697ac657480c23
Max Base <MaxBaseCode@Gmail.Com>
2025-03-14 14:31:02 +0330

    Update ssh_tunnel_keeper.sh

--- ssh_tunnel_keeper.sh
+++ ssh_tunnel_keeper.sh
@@ -4,6 +4,12 @@
 # https://github.com/BaseMax/persistent-ssh-proxy
 # Keep a SOCKS5 tunnel via SSH in the background alive
 
+SSH_HOST="your.server.com"
+SSH_USER="root"
+SSH_PASSWORD="yourpassword"
+SSH_PORT=22
+SOCKS_PORT=9999
+
 if ! command -v ssh &> /dev/null; then
     echo "Error: ssh is not installed. Please install it first."
     exit 1
@@ -15,8 +21,8 @@ if ! command -v sshpass &> /dev/null; then
 fi
 
 while true; do
-    sshpass -p "******" ssh -D 9999 -q -C -N root@******
+    sshpass -p "$SSH_PASSWORD" ssh -D $SOCKS_PORT -q -C -N -p $SSH_PORT $SSH_USER@$SSH_HOST
     
-    echo "SSH connection lost. Reconnecting in 15 seconds..."
+    echo "SSH connection lost. Reconnecting in 5 seconds..."
     sleep 5
 done
